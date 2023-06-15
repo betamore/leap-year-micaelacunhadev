2e9aa3406dac6677e308a95aa84ecf69de2da73f
micaelacunhadev <102747966+micaelacunhadev@users.noreply.github.com>
2023-06-15 18:33:46 -0400

    Added the function keyword to isLeapYear()

--- leap-year.js
+++ leap-year.js
@@ -1,7 +1,17 @@
-isLeapYear(){
+//The tricky thing here is that a leap year in the Gregorian calendar occurs:
+//on every year that is evenly divisible by 4 except every year that is evenly divisible by 100 unless the year is also evenly divisible by 400
 
-	// Write code here
-	
+//For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+function isLeapYear(year){
+
+	if ( year%4 !== 0 ){
+		return false
+	}
+	else{
+		if ( year%100 === 0 && year%400 === 0)
+			return true
+	}
+	return false
 }
 
 
@@ -10,5 +20,5 @@ isLeapYear(){
 let years = [ 2015, 1970, 1996, 1960, 2100, 1900, 2000, 2400, 1800]
 
 for (let year of years) {
-	console.log(isLeapYear(year)
+	console.log(isLeapYear(year))
 }
